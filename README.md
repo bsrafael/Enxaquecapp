455c19ce6654d115b1bffe2a433ee5a02f93de12
bsrafael <rafaelbs159@gmail.com>
2020-05-02 15:45:43 -0300

    alpha: login & register layouts

---
+++ README.md
@@ -0,0 +1,7 @@
+# Enxaquecapp
+
+## THIS IS A WORK IN PROGRESS
+
+## Contributors
+- [Rafael](https://github.com/bsrafael)
+- [Julio](https://github.com/OtherRandomUser)
---
+++ app/src/main/java/com/example/enxaquecapp/presentation/register/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.enxaquecapp.presentation.register
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.enxaquecapp.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
