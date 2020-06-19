6837bb9e8d1904c15d2bf9dfb16cdabf44e4b32e
bsrafael <rafaelbs159@gmail.com>
2020-06-19 00:01:50 -0300

    default implementation for noContent

--- app/src/main/java/com/enxaquecapp/app/api/ApiCallback.kt
+++ app/src/main/java/com/enxaquecapp/app/api/ApiCallback.kt
@@ -1,10 +1,12 @@
 package com.enxaquecapp.app.api
 
+import android.util.Log
+
 interface ApiCallback<T> {
 
     fun success(response: T)
 
-    fun noContent()
+    fun noContent() { Log.i("ApiCallback", "no content") }
 
     fun failure(errorCode: Int, message: String)
 
